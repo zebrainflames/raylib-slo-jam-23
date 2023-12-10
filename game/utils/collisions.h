35151ffd725bba60f993b08e8c4a59adce381f13
jhkroger <jhkroger2@gmail.com>
2023-12-10 19:54:20 +0200

    removed external deps that should not be used in the jam perhaps

--- game/utils/collisions.h
+++ game/utils/collisions.h
@@ -1,19 +1,10 @@
-//
-// Created by greyfriar on 20.3.2021.
-//
-
-#ifndef INC_0X37_EVALUATE_RAYLIB_COLLISIONS_H
-#define INC_0X37_EVALUATE_RAYLIB_COLLISIONS_H
+#pragma once
 
 #include <raylib.h>
-#include "cute_c2.h"
 
-c2AABB rectToAABB(Rectangle a);
+typedef struct {
+    float depth;
+    float nx, ny;
+} collision_result;
 
-c2Manifold getColManifold(Rectangle a, Rectangle b);
-
-void resolveRects(Rectangle* a, Rectangle* b, c2Manifold m);
-
-void drawManifold(c2Manifold m);
-
-#endif // INC_0X37_EVALUATE_RAYLIB_COLLISIONS_H
+collision_result resolveRects(Rectangle* a, Rectangle* b);
